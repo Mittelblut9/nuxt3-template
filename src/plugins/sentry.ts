bb2b54971ea41c2b645cb475b14b215d4656b958
Mittelblut9 <sauer.benedikt@outlook.de>
2024-01-06 17:56:47 +0100

    feat: update client sentry

--- src/plugins/sentry.ts
+++ src/plugins/sentry.ts
@@ -1,7 +1,6 @@
-import * as Sentry from '@sentry/vue';
+import * as Sentry from '@sentry/browser';
 
-export default defineNuxtPlugin((nuxtApp) => {
-    const router = useRouter();
+export default defineNuxtPlugin(() => {
     const {
         public: { sentryDsn },
     } = useRuntimeConfig();
@@ -11,20 +10,21 @@ export default defineNuxtPlugin((nuxtApp) => {
     }
 
     Sentry.init({
-        app: nuxtApp.vueApp,
         dsn: sentryDsn,
         environment: process.env.NODE_ENV,
         integrations: [
-            new Sentry.BrowserTracing({
-                routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+            new Sentry.BrowserTracing(),
+            new Sentry.BrowserProfilingIntegration(),
+            new Sentry.Replay({
+                maskAllText: true,
+                blockAllMedia: true,
             }),
-            new Sentry.Replay(),
         ],
         tracesSampleRate: 0.25,
-        tracePropagationTargets: ['https://projects.blackdayz.de'],
-        profilesSampleRate: 0.25,
+        tracePropagationTargets: ['https://projects.blackdayz.de', 'http://127.0.0.1:3000'],
+        profilesSampleRate: 0.5,
 
-        replaysSessionSampleRate: 0.25,
+        replaysSessionSampleRate: 0.5,
         replaysOnErrorSampleRate: 0.5,
     });
 });
