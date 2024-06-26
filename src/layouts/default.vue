81b22ae3e04b57fc4935d48187c12373ea61515c
Mittelblut9 <sauer.benedikt@outlook.de>
2024-06-26 12:41:27 +0200

    feat(BD-18): add default layout with toast notification

---
+++ src/layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="dark">
+        <NuxtPage />
+        <UNotifications>
+            <template #title="{ title }">
+                <span
+                    class="text-black"
+                    v-html="title"
+                />
+            </template>
+
+            <template #description="{ description }">
+                <span
+                    class="text-black"
+                    v-html="description"
+                />
+            </template>
+        </UNotifications>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'DefaultLayout',
+};
+</script>
